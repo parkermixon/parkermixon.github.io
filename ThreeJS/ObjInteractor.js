b3b7d6440e6f9728809cf6e9380a8359a9e059be
parkermixon <71618148+parkermixon@users.noreply.github.com>
2024-11-05 15:09:57 -0500

    added no XR version and improved controls/guis

---
+++ ThreeJS/ObjInteractor.js
@@ -0,0 +1,292 @@
+import * as THREE from 'three';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import ThreeMeshUI from "three-mesh-ui";
+
+export const ObjectInteractor = class {
+
+    rotInputScale = 10;
+    zoomInputScale = 20;
+    playbackInputScale = 0.1; // as per frame
+
+    minPlaybackSpeed = 0.1;
+    maxPlaybackSpeed = 2;
+
+    #time = 0;
+    set time(val) {
+        this.#time = val;
+        if (this.mode == 'xr') {
+            this.timeLabel.set({
+                content: "Time: " + val.toFixed(2).toString() + "s"
+            });
+        } else {
+            this.timeLabel.innerText = "Time: " + val.toFixed(2).toString() + "s";
+        }
+    }
+
+    get time() { 
+        return this.#time;
+    }
+
+    #playbackSpeed = 1;
+    set playbackSpeed(val) { 
+        this.#playbackSpeed = val;
+
+        if (this.mode == 'xr') {
+            this.speedLabel.set({
+                content: "Speed: " + val.toFixed(2).toString() + "x"
+            });
+        } else {
+            this.speedLabel.innerText = "Speed: " + val.toFixed(2).toString() + "x";
+        }
+    }
+
+    get playbackSpeed() { 
+        return this.#playbackSpeed;
+    }
+
+    // Todo change constructor to just set up vars, then make an init
+    constructor(isXRSupported, interactionObj, initDist, noXrControlTemplate, width = window.innerWidth, height = window.innerHeight) {
+        this.mode = (isXRSupported) ? 'xr' : 'noxr';
+        this.noXrControlTemplate = noXrControlTemplate;
+        
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setSize( width, height );
+        document.body.appendChild( this.renderer.domElement );
+
+        this.interactionObj = interactionObj.clone();
+        this.scene.add(this.interactionObj);
+
+        //Variables to be defined by init functions
+        this.controlGui = null;
+        this.timeLabel = null;
+        this.speedLabel = null;
+
+        if (isXRSupported) {
+            // Enable vr & apply vr settings
+            this.renderer.xr.enabled = true;
+            this.#initXR();
+            this.interactionObj.position.set(
+                this.camera.position.x, 
+                this.camera.position.y, 
+                this.camera.position.z - initDist,
+            );
+        } else {
+            this.#initNoXR();
+            this.interactionObj.position.set(
+                this.camera.position.x, 
+                this.camera.position.y, 
+                this.camera.position.z,
+            );
+    
+            this.camera.position.z = initDist;
+        }
+
+        this.playbackSpeed = 1;
+        this.time = 0;
+
+        document.body.appendChild(VRButton.createButton(this.renderer));
+        this.renderer.setAnimationLoop(() => {
+            if (isXRSupported) {
+                this.#pollControllerInputs();
+                ThreeMeshUI.update();
+            } else {
+
+            }
+            
+            this.renderer.render(this.scene, this.camera);
+        });
+    }
+
+    #initXR() {
+        // Create Gui
+        const container = this.#createGui();
+
+        this.scene.add(container);
+
+        this.controlGui = container;
+
+        // Load Controllers
+
+        this.renderer.xr.addEventListener('sessionstart', ()=> {
+            console.log('Starting XR Session')
+    
+            // Init right controller
+            this.rightController = this.renderer.xr.getController(0);
+            this.rightController.name = 'right';
+            this.rightController.isSelected = false;
+            this.rightController.isGripped = false;
+    
+            this.rightController.addEventListener( 'connected', (e) => {
+                this.rightController.gamepad = e.data.gamepad
+            });
+    
+            this.rightController.addEventListener("selectstart", (event) => {
+                this.rightController.isSelected = true;
+                this.rightController.prevPosition = this.rightController.position.clone();
+            });
+    
+            this.rightController.addEventListener("selectend", (event) => {
+                this.rightController.isSelected = false;
+                this.rightController.prevPosition.set(0, 0, 0);
+            })
+    
+            this.rightController.addEventListener("squeezestart", (event) => {
+                this.rightController.isGripped = true;
+                this.rightController.prevPosition = this.rightController.position.clone();
+            });
+    
+            this.rightController.addEventListener("squeezeend", (event) => {
+                this.rightController.isGripped = false;
+                this.rightController.prevPosition.set(0, 0, 0);
+            })
+    
+            // Init left controller
+            this.leftController = this.renderer.xr.getController(1);
+            this.leftController.name = 'left';
+            this.leftController.isSelected = false;
+            this.leftController.isGripped = false;
+    
+            this.leftController.addEventListener( 'connected', (e) => {
+                this.leftController.gamepad = e.data.gamepad
+            });
+            
+            this.leftController.addEventListener("selectstart", (event) => {
+                this.leftController.isSelected = true;
+                this.leftController.prevPosition = this.leftController.position.clone();
+            });
+    
+            this.leftController.addEventListener("selectend", (event) => {
+                this.leftController.isSelected = false;
+                this.leftController.prevPosition.set(0, 0, 0);
+            });
+    
+            this.leftController.addEventListener("squeezestart", (event) => {
+                this.leftController.isGripped = true;
+                this.leftController.prevPosition = this.leftController.position.clone();
+            });
+    
+            this.leftController.addEventListener("squeezeend", (event) => {
+                this.leftController.isGripped = false;
+                this.leftController.prevPosition.set(0, 0, 0);
+            })
+        });
+    
+        this.renderer.xr.addEventListener('sessionend', () => {
+            this.leftController = null;
+            this.rightController = null;
+        });
+    }
+
+    #initNoXR() {
+
+        this.controlGui = document.body.appendChild(this.noXrControlTemplate.content);
+        // Yes, this will ignore the id in the template, only finding for inserted element
+        this.timeLabel = document.getElementById("timeLabel");
+        this.speedLabel = document.getElementById("speedLabel");
+
+        let speedControl = document.getElementById("speedControl");
+        speedControl.min = this.minPlaybackSpeed;
+        speedControl.max = this.maxPlaybackSpeed;
+        speedControl.step = this.playbackInputScale;
+        speedControl.value = this.playbackSpeed;
+        speedControl.addEventListener("change", (e)=> {
+            this.playbackSpeed = parseFloat(e.target.value);
+        });
+
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.update();
+    }
+
+    #pollControllerInputs() {
+        // defaults to right controller
+        if (this.rightController) {
+            if (this.rightController.isSelected) {
+                let deltaPos = this.rightController.prevPosition.clone().sub(this.rightController.position);
+                // rotate y on local axis
+                this.interactionObj.rotateY(-deltaPos.x * this.rotInputScale);
+                // rotate x on world axis
+                this.interactionObj.rotateOnWorldAxis(new THREE.Vector3(-1, 0, 0), -deltaPos.y * this.rotInputScale);
+            }
+
+            if (this.rightController && this.rightController.gamepad) {
+                // axis 2 is x
+                let playback_val = this.rightController.gamepad.axes[2] || this.rightController.gamepad.axes[0];
+                playback_val *= this.playbackInputScale;
+                this.playbackSpeed += playback_val / 60;
+                this.playbackSpeed = Math.min(this.maxPlaybackSpeed, Math.max(this.playbackSpeed, this.minPlaybackSpeed));
+                // divide by 60 to attempt to adjust to framerate
+            }
+
+        } else if (this.leftController) {
+            if (this.leftController.isSelected) {
+                let deltaPos = this.leftController.prevPosition.clone().sub(this.leftController.position);
+                // rotate y on local axis
+                this.interactionObj.rotateY(-deltaPos.x * this.rotInputScale);
+                // rotate x on world axis
+                this.interactionObj.rotateOnWorldAxis(new THREE.Vector3(-1, 0, 0), -deltaPos.y * this.rotInputScale);
+            }
+        }
+
+        if (this.rightController && this.leftController) {
+            if (this.rightController.isGripped && this.leftController.isGripped) {
+                let prevDist = this.leftController.prevPosition.sub(this.rightController.prevPosition).length();
+                let currentDist = this.leftController.position.clone().sub(this.rightController.position).length();
+                let zoomVal = currentDist - prevDist;
+                this.interactionObj.translateZ(zoomVal * this.zoomInputScale);
+            }
+        }
+
+        if (this.rightController) {
+            this.rightController.prevPosition = this.rightController.position.clone();
+        }
+        if (this.leftController) { 
+            this.leftController.prevPosition = this.leftController.position.clone();
+        }
+    }
+
+    #createGui() {
+        const container = new ThreeMeshUI.Block({
+            width: 1.2,
+            height: 0.7,
+            padding: 0.2,
+            fontFamily: '../assets/Roboto-msdf.json',
+            fontTexture: '../assets/Roboto-msdf.png',
+        });
+    
+        let timePanel = new ThreeMeshUI.Block({
+            width: 0.6,
+            height: 0.2,
+            fontFamily: '../assets/Roboto-msdf.json',
+            fontTexture: '../assets/Roboto-msdf.png',
+            backgroundOpacity: 0
+        })
+        
+        this.timeLabel = new ThreeMeshUI.Text({
+            content: "Time: "
+        });
+    
+        timePanel.add(this.timeLabel);
+        container.add(timePanel);
+    
+        let playbackSpeedPanel = new ThreeMeshUI.Block({
+            width: 0.6,
+            height: 0.2,
+            fontFamily: '../assets/Roboto-msdf.json',
+            fontTexture: '../assets/Roboto-msdf.png',
+            backgroundOpacity: 0
+        })
+    
+        this.speedLabel = new ThreeMeshUI.Text({
+            content: "Speed: 1"
+        });
+    
+        playbackSpeedPanel.add(this.speedLabel);
+        container.add(playbackSpeedPanel);
+
+        return container;
+    }
+}
--- ThreeJS/script.js
+++ ThreeJS/script.js
@@ -1,245 +1,45 @@
-import * as THREE from 'three';
-import { VRButton } from 'three/addons/webxr/VRButton.js';
+import { ObjectInteractor } from "./ObjInteractor.js";
 import { VTKLoader } from 'three/addons/loaders/VTKLoader.js';
-import ThreeMeshUI from "three-mesh-ui";
-
-//TODO: if I implement server side stuff, could use vtk.js to read file, then serve back vtk model and the vertex values to be animated on the client side
+import * as THREE from 'three';
 
 const importMeshSize = 10;
-const rotInputScale = 10;
-const zoomInputScale = 20;
-const playbackInputScale = 0.1; // as per frame
+const noXrTemplate = document.getElementById("noXrControlGui");
 
-const minPlaybackSpeed = 0.1;
-const maxPlaybackSpeed = 2;
+var objInteractor;
 
-var playbackSpeed = 1;
+const loader = new VTKLoader();
+loader.load( '../data/vtk_export.vtk', function ( geometry ) {
 
-var renderer, camera, scene, interactionObj;
-var leftController = null;
-var rightController = null;
-var container;
-var timeText;
-var playbackSpeedText;
+    geometry.center();
+    geometry.computeVertexNormals();
 
-function init() {
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 100 );
-    scene = new THREE.Scene();
-    scene.add(camera);
+    const material = new THREE.MeshBasicMaterial( { color: 0xffffff });
+    material.side = THREE.DoubleSide;
+    material.vertexColors = true;
+    const mesh = new THREE.Mesh( geometry, material );
 
-    container = new ThreeMeshUI.Block({
-        width: 1.2,
-        height: 0.7,
-        padding: 0.2,
-        fontFamily: './assets/Roboto-msdf.json',
-        fontTexture: './assets/Roboto-msdf.png',
+    const interactionObj = new THREE.Group();
+    interactionObj.add(mesh);
+    
+    // Have to realign this mesh due to different coord systems
+    mesh.rotateX(-90);
+
+    let scale = importMeshSize / geometry.boundingBox.max.sub(geometry.boundingBox.min).length();
+    mesh.scale.multiplyScalar(scale);
+    let initDist = Math.abs(geometry.boundingBox.max.z) * scale * 2 * 1.5;
+    
+    navigator.xr.isSessionSupported('immersive-vr').then((isSupported) => {
+        objInteractor = new ObjectInteractor(isSupported, interactionObj, initDist, noXrTemplate);
     });
-
-    const timePanel = new ThreeMeshUI.Block({
-        width: 0.6,
-        height: 0.2,
-        fontFamily: './assets/Roboto-msdf.json',
-        fontTexture: './assets/Roboto-msdf.png',
-        backgroundOpacity: 0
+    
+    fetch('../data/uter_out.json').then((response) => response.json()).then((json_data) => {
+        let vertex_vals = json_data.pointValues;
+        let vertex_colors = json_data.colors;
+        let time = json_data.time;
+        anim_vertex_colors(geometry, vertex_colors, time);
     })
     
-    timeText = new ThreeMeshUI.Text({
-        content: "Time: "
-    });
-
-    timePanel.add(timeText);
-    container.add(timePanel);
-
-    const playbackSpeedPanel = new ThreeMeshUI.Block({
-        width: 0.6,
-        height: 0.2,
-        fontFamily: './assets/Roboto-msdf.json',
-        fontTexture: './assets/Roboto-msdf.png',
-        backgroundOpacity: 0
-    })
-
-    playbackSpeedText = new ThreeMeshUI.Text({
-        content: "Speed: 1"
-    });
-
-    playbackSpeedPanel.add(playbackSpeedText);
-    container.add(playbackSpeedPanel);
-
-    container.position.copy(camera.position)
-    container.position.setZ(container.position.z - 2);
-    container.position.setY(0);
-
-    scene.add(container);
-
-    // Renderer
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    
-    // Enable vr & apply vr settings
-    document.body.appendChild( VRButton.createButton(renderer));
-    renderer.xr.enabled = true;
-
-    
-
-    // TODO: turn controller set up into functions
-    renderer.xr.addEventListener('sessionstart', ()=> {
-        console.log('starting session')
-
-        // Init right controller
-
-        rightController = renderer.xr.getController(0);
-        rightController.name = 'right';
-        rightController.isSelected = false;
-        rightController.isGripped = false;
-
-        rightController.addEventListener( 'connected', (e) => {
-            rightController.gamepad = e.data.gamepad
-        });
-
-        rightController.addEventListener("selectstart", (event) => {
-            rightController.isSelected = true;
-            rightController.prevPosition = rightController.position.clone();
-        });
-
-        rightController.addEventListener("selectend", (event) => {
-            rightController.isSelected = false;
-            rightController.prevPosition.set(0, 0, 0);
-        })
-
-        rightController.addEventListener("squeezestart", (event) => {
-            rightController.isGripped = true;
-            rightController.prevPosition = rightController.position.clone();
-        });
-
-        rightController.addEventListener("squeezeend", (event) => {
-            rightController.isGripped = false;
-            rightController.prevPosition.set(0, 0, 0);
-        })
-
-        // Init left controller
-        leftController = renderer.xr.getController(1);
-        leftController.name = 'left';
-        leftController.isSelected = false;
-        leftController.isGripped = false;
-
-        leftController.addEventListener( 'connected', (e) => {
-            leftController.gamepad = e.data.gamepad
-        });
-        
-        leftController.addEventListener("selectstart", (event) => {
-            leftController.isSelected = true;
-            leftController.prevPosition = leftController.position.clone();
-        });
-
-        leftController.addEventListener("selectend", (event) => {
-            leftController.isSelected = false;
-            leftController.prevPosition.set(0, 0, 0);
-        });
-
-        leftController.addEventListener("squeezestart", (event) => {
-            leftController.isGripped = true;
-            leftController.prevPosition = leftController.position.clone();
-        });
-
-        leftController.addEventListener("squeezeend", (event) => {
-            leftController.isGripped = false;
-            leftController.prevPosition.set(0, 0, 0);
-        })
-        renderer.setAnimationLoop(animate);
-    });
-
-    renderer.xr.addEventListener('sessionend', () => {
-        leftController = null;
-        rightController = null;
-    });
-
-    // Load vtk file
-    const loader = new VTKLoader();
-    loader.load( 'data/vtk_export.vtk', function ( geometry ) {
-
-        geometry.center();
-        geometry.computeVertexNormals();
-
-        const material = new THREE.MeshBasicMaterial( { color: 0xffffff });
-        material.side = THREE.DoubleSide;
-        material.vertexColors = true;
-        const mesh = new THREE.Mesh( geometry, material );
-
-        interactionObj = new THREE.Group();
-        interactionObj.add(mesh);
-        mesh.rotateX(-90);
-
-        let scale = importMeshSize / geometry.boundingBox.max.sub(geometry.boundingBox.min).length();
-        mesh.scale.multiplyScalar(scale);
-        let dist = Math.abs(geometry.boundingBox.max.z) * scale * 2 * 1.5;
-        interactionObj.position.set(camera.position.x, camera.position.y, camera.position.z - dist);
-        
-        scene.add(interactionObj);
-        
-        fetch('data/uter_out.json').then((response) => response.json()).then((json_data) => {
-            let vertex_vals = json_data.pointValues;
-            let vertex_colors = json_data.colors;
-            let time = json_data.time;
-            anim_vertex_colors(geometry, vertex_colors, time);
-        })
-
-    } );
-
-    document.body.appendChild( renderer.domElement );
-}
-
-function animate() {
-    // defaults to right controller
-    if (rightController) {
-        if (rightController.isSelected) {
-            console.log(rightController.prevPosition);
-            let deltaPos = rightController.prevPosition.clone().sub(rightController.position);
-            // rotate y on local axis
-            interactionObj.rotateY(-deltaPos.x * rotInputScale);
-            // rotate x on world axis
-            interactionObj.rotateOnWorldAxis(new THREE.Vector3(-1, 0, 0), -deltaPos.y * rotInputScale);
-        }
-
-        if (rightController.gamepad) {
-            // axis 2 is x
-            let playback_val = rightController.gamepad.axes[2] * playbackInputScale;
-            playbackSpeed += playback_val / 60;
-            playbackSpeed = Math.min(maxPlaybackSpeed, Math.max(playbackSpeed, minPlaybackSpeed));
-            // divide by 60 to attempt to adjust to framerate
-        }
-
-    } else if (leftController) {
-        if (leftController.isSelected) {
-            let deltaPos = leftController.prevPosition.clone().sub(leftController.position);
-            // rotate y on local axis
-            interactionObj.rotateY(-deltaPos.x * rotInputScale);
-            // rotate x on world axis
-            interactionObj.rotateOnWorldAxis(new THREE.Vector3(-1, 0, 0), -deltaPos.y * rotInputScale);
-        }
-    }
-
-    if (rightController && leftController) {
-        if (rightController.isGripped && leftController.isGripped) {
-            let prevDist = leftController.prevPosition.sub(rightController.prevPosition).length();
-            let currentDist = leftController.position.clone().sub(rightController.position).length();
-            let zoomVal = currentDist - prevDist;
-            interactionObj.translateZ(zoomVal * zoomInputScale);
-            
-            console.log(interactionObj.position.z)
-        }
-    }
-
-    if (rightController) {
-        rightController.prevPosition = rightController.position.clone();
-    }
-    if (leftController) { 
-        leftController.prevPosition = leftController.position.clone();
-    }
-
-    ThreeMeshUI.update();
-    renderer.render(scene, camera);
-}
+} );
 
 async function anim_vertex_colors(geo, vertex_colors, time_values) {
     let time_idx = 0;
@@ -256,17 +56,14 @@ async function anim_vertex_colors(geo, vertex_colors, time_values) {
             deltaTime = 1000;
         }
 
-        timeText.set({content: "Time: " + time_values[time_idx].toFixed(2).toString() + "s"});
-        playbackSpeedText.set({content: "Speed: " + playbackSpeed.toFixed(2) + "x"});
+        objInteractor.time = time_values[time_idx];
         
         time_idx += 1;
+        // Loop if greater than time
         if (time_idx > time_values.length-1) {
             time_idx = 0;
-            console.log('looping');
         }
 
-        await new Promise(r => setTimeout(r, deltaTime/playbackSpeed));
+        await new Promise(r => setTimeout(r, deltaTime/objInteractor.playbackSpeed));
     }
 }
-
-init();
